5ee5212d5fe84b0ec319632b708a1b31a07c612b
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-24 14:23:36 +0600

    login registration done

--- src/Components/Auth/Register.jsx
+++ src/Components/Auth/Register.jsx
@@ -1,10 +1,12 @@
 import PropTypes from "prop-types";
+import { FcGoogle } from "react-icons/fc";
 import { useContext, useState } from "react";
 import { GlobalDataContext } from "../../ContextApi/DataContext";
 import { toast } from "react-toastify";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "flowbite-react";
 import { AiOutlineLoading } from "react-icons/ai";
+import { ButtonLoading, ButtonPrimary } from "../Shared/Buttons";
 
 const Register = ({ setPageToggle }) => {
   const [creatingAccount, setCreatingAccount] = useState(false);
@@ -151,30 +153,7 @@ const Register = ({ setPageToggle }) => {
                     }}
                     className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                   >
-                    <svg
-                      className="w-4 h-auto"
-                      width="46"
-                      height="47"
-                      viewBox="0 0 46 47"
-                      fill="none"
-                    >
-                      <path
-                        d="M46 24.0287C46 22.09 45.8533 20.68 45.5013 19.2112H23.4694V27.9356H36.4069C36.1429 30.1094 34.7347 33.37 31.5957 35.5731L31.5663 35.8669L38.5191 41.2719L38.9885 41.3306C43.4477 37.2181 46 31.1669 46 24.0287Z"
-                        fill="#4285F4"
-                      />
-                      <path
-                        d="M23.4694 47C29.8061 47 35.1161 44.9144 39.0179 41.3012L31.625 35.5437C29.6301 36.9244 26.9898 37.8937 23.4987 37.8937C17.2793 37.8937 12.0281 33.7812 10.1505 28.1412L9.88649 28.1706L2.61097 33.7812L2.52296 34.0456C6.36608 41.7125 14.287 47 23.4694 47Z"
-                        fill="#34A853"
-                      />
-                      <path
-                        d="M10.1212 28.1413C9.62245 26.6725 9.32908 25.1156 9.32908 23.5C9.32908 21.8844 9.62245 20.3275 10.0918 18.8588V18.5356L2.75765 12.8369L2.52296 12.9544C0.909439 16.1269 0 19.7106 0 23.5C0 27.2894 0.909439 30.8731 2.49362 34.0456L10.1212 28.1413Z"
-                        fill="#FBBC05"
-                      />
-                      <path
-                        d="M23.4694 9.07688C27.8699 9.07688 30.8622 10.9863 32.5344 12.5725L39.1645 6.11C35.0867 2.32063 29.8061 0 23.4694 0C14.287 0 6.36607 5.2875 2.49362 12.9544L10.0918 18.8588C11.9987 13.1894 17.25 9.07688 23.4694 9.07688Z"
-                        fill="#EB4335"
-                      />
-                    </svg>
+                    <FcGoogle className="text-xl" />
                     Sign in with Google
                   </button>
 
@@ -207,20 +186,19 @@ const Register = ({ setPageToggle }) => {
                       {/* Form Group Image URL */}
                       <div>
                         <label
-                          htmlFor="name"
-                          className="block text-sm mb-2 dark:text-white"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          htmlFor="file_input"
                         >
-                          Image URL
+                          Upload file
                         </label>
-                        <div className="relative">
-                          <input
-                            type="text"
-                            placeholder="Enter Image URL..."
-                            name="imageUrl"
-                            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                            required
-                          />
-                        </div>
+                        <input
+                          className="file:border-0
+                          !file:bg-gray-100 !file:me-4
+                          !file:py-3 file:px-4
+                          dark:file:bg-gray-700 dark:file:text-gray-400"
+                       
+                          type="file"
+                        />
                       </div>
                       {/* End Form Group */}
 
@@ -288,16 +266,9 @@ const Register = ({ setPageToggle }) => {
                       )}
                       {/* display login error */}
 
-                      <Button size="md" type="submit">
-                        {creatingAccount ? (
-                          <div className="flex justify-center items-center gap-2">
-                            <AiOutlineLoading className="h-6 w-6 animate-spin" />{" "}
-                            Creating{" "}
-                          </div>
-                        ) : (
-                          "Sign Up"
-                        )}
-                      </Button>
+                      <ButtonLoading isLoading={false} size="md" type="submit">
+                       Sign Up
+                      </ButtonLoading>
                     </div>
                   </form>
                 </div>
--- src/Components/Shared/Buttons.jsx
+++ src/Components/Shared/Buttons.jsx
@@ -1,8 +1,9 @@
 import { FaArrowRight, FaSpinner} from "react-icons/fa6";
+import PropTypes from "prop-types";
 
 const ButtonPrimary = ({children}) => {
   return (
-    <div className="bg-[#FF6636] px-3 py-2 text-white font-bold rounded-lg hover:bg-[#CC522B] shadow-sm transition-all ease-in-out">
+    <div className="bg-[#FF6636] flex justify-center items-center px-3 py-2 text-white font-bold rounded-lg hover:bg-[#CC522B] shadow-sm transition-all ease-in-out">
       {children}
     </div>
   )
@@ -26,13 +27,26 @@ const ButtonSecondary = ({children}) => {
 
   const ButtonLoading = ({children, isLoading}) => {
     return (
-      <div className="bg-[#FFEEE8] px-3 py-2 text-[#FF6636] font-bold rounded-lg hover:bg-[#FFDDD1] shadow-sm transition-all ease-in-out">
+      <div className="px-3 py-2 bg-[#FF6636] font-bold rounded-lg hover:bg-[#CC522B] text-white shadow-sm transition-all ease-in-out">
         <div className="flex gap-5 justify-center items-center">
-            {isLoading ? <FaSpinner className="animate-spin" /> :  children }
+            {isLoading ? <FaSpinner className="animate-spin text-2xl" /> :  children }
            
             </div>
       </div>
     )
   }
-
+  ButtonPrimary.propTypes = {
+    children: PropTypes.func,
+  };
+  ButtonSecondary.propTypes = {
+    children: PropTypes.func,
+  };
+  ButtonArrow.propTypes = {
+    children: PropTypes.func,
+  };
+  ButtonLoading.propTypes = {
+    children: PropTypes.func,
+    isLoading: PropTypes.bool
+  };
+  
 export {ButtonPrimary, ButtonSecondary, ButtonArrow, ButtonLoading}

c00f8dfff53e05d3c5c2f3d674d9f69f8fbbb854
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-24 18:57:02 +0600

    nav added

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
-// import Nav from "../Components/Nav/Nav";
+import { ComplexNavbar } from "../Components/Nav/Nav";
 // import FooterSection from "../Components/Footer/FooterSection";
 
 const MainLayout = () => {
   return (
     <div className="dark:bg-gray-900">
       <div className=" max-w-[1280px] mx-auto px-5 lg:px-10">
-        {/* <Nav></Nav> */}
+        <div className="mt-5 w-full">
+          <ComplexNavbar></ComplexNavbar>
+        </div>
         <Outlet></Outlet>
         {/* <FooterSection></FooterSection> */}
       </div>
